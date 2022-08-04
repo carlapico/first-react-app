92a1f66c85fa859114e62a4a689dbfd6b62b9fdd
Carla Pico <carlapico@Carlas-MacBook-Air.local>
2022-08-04 14:09:03 -0400

    now that button counts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import {useState } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let count = 0;
+  // let count = 0; 
+  const[count,setCount] = useState(0) // what it starts with and in the future you will pass other numbers through the parameter 0 
+  //and creating it into a function and setting it to the two variables that will contain the value of what we are keeping track of 
+  //and setting the value. 
   return (
     <div className="App">
       <header className="App-header">
@@ -12,9 +17,10 @@ function App() {
          You clicked the button {count} times.
         </p>
         <button onClick={ () => {
-          console.log('I clicked the button');
-          count = count +1;
-        }}>Click Me</button>
+          console.log('I clicked the button ' + count);
+          // count = count +1;
+          setCount(count + 1) // we can set it to a specific number or a calculation.
+        }}>Add One</button>
       </header>
     </div>
   );
